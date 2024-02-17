ceefc91980955117512472a28d3818901508b68a
genuine_code <113706866+genuinecode1@users.noreply.github.com>
2024-02-17 21:46:23 +0530

    17 Feb

---
+++ 1642. Furthest Building You Can Reach.cpp
@@ -0,0 +1,48 @@
+
+
+/*
+
+Author :- Aditya Mani Tripathi
+leetcode username - genuine_code
+email - aadiitripathii46@gmail.com
+institute - UIET CSJM UNIVERSITY KANPUR
+
+*/
+
+
+
+// code :-
+
+
+class Solution {
+public:
+    int furthestBuilding(vector<int>& h, int b, int l) {
+        priority_queue<int> p;
+    
+        int i=0, diff =0; 
+        for(i=0; i<h.size()-1; i++){ 
+            
+            diff = h[i+1]-h[i];
+            
+            if(diff <= 0){
+                continue;
+            }
+
+            b -= diff; 
+            p.push(diff); 
+       
+            if(b < 0){
+                b += p.top(); 
+                p.pop(); 
+                l--;
+            }
+
+          
+            if(l < 0) break;
+        }
+        
+        
+        return i;
+        
+    }
+};
